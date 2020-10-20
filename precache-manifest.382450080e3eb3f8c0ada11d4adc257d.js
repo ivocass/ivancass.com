0d238184a004890e06c7a4519f0a8ff7baacd1c2
ivocass <ivoasdf@gmail.com>
2020-10-20 20:08:38 -0300

    Updates

---
+++ precache-manifest.382450080e3eb3f8c0ada11d4adc257d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc3909be50bad329fe3e3ec95a99ef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c49d08d8b049e99c360",
+    "url": "/static/css/main.6827bd29.chunk.css"
+  },
+  {
+    "revision": "e6859c553b960ce136c4",
+    "url": "/static/js/2.3ec3b606.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ec3b606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c49d08d8b049e99c360",
+    "url": "/static/js/main.9922d130.chunk.js"
+  },
+  {
+    "revision": "0822f86dc269d991fe50",
+    "url": "/static/js/runtime-main.f6f67ac1.js"
+  }
+]);
