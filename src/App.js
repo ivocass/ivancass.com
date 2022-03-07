8d696c2db4c7a720d8d42cfdce5f463550231f9f
ivocass <ivoasdf@gmail.com>
2022-03-07 14:01:53 -0300

    20220307

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+
+
+
+export default class App extends Component {
+	constructor() {
+		
+		super()
+	}
+
+
+
+
+	render() {
+		
+		return <div id="App">😴</div>;
+	}
+}
